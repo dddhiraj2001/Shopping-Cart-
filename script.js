2438e4e13534d73a18590a22d7d97d5590fc5853
dddhiraj2001 <dddhiraj2001@gmail.com>
2024-02-15 22:05:27 -0500

    Clear UI Functionality added.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
 const clearAll = document.getElementById('clear')
+
+const filter = document.querySelector('.filter')
 const addItem = e => {
     e.preventDefault()
 
@@ -12,7 +14,8 @@ const addItem = e => {
     console.log('Sucess');
     const child = createDiv(itemInput.value)
     itemList.appendChild(child)
-    console.log(itemInput.value)
+    checkUI();
+    // console.log(itemInput.value)
     itemInput.value=''
     
 }
@@ -24,10 +27,10 @@ const createDiv = shoppingItem =>{
     const text_node =  document.createTextNode(shoppingItem)
     crossButton.className = "remove-item btn-link text-red"
     iStyle.className = "fa-solid fa-xmark"
-    console.log(crossButton.outerHTML)
+    // console.log(crossButton.outerHTML)
     crossButton.setAttribute('class',"remove-item btn-link text-red")
     iStyle.setAttribute('class',"fa-solid fa-xmark") 
-    console.log(crossButton.outerHTML)
+    // console.log(crossButton.outerHTML)
     listItem.appendChild(text_node)
     listItem.appendChild(crossButton)
     crossButton.appendChild(iStyle)
@@ -40,10 +43,29 @@ const removeItem = e => {
     // console.log(e.target.tagName)
     // console.log(e.target.parentElement)
     if (e.target.tagName === "I"){
+        if(confirm('Are you sure')){
         e.target.parentElement.parentElement.remove();
+        checkUI();
     }
 }
-const emptyList = () =>  itemList.innerHTML = " " 
+}
+const emptyList = () =>  {
+    itemList.innerHTML = " " ;
+    checkUI();
+}
+const checkUI = () => {
+    const items = document.querySelectorAll('li')
+    console.log(items.length)
+    if(items.length===0){
+        filter.style.display = 'none'
+        clearAll.style.display = 'none'
+    }else{
+        filter.style.display = 'block'
+        clearAll.style.display = 'block'
+    }
+}
 itemForm.addEventListener('submit',addItem)
 itemList.addEventListener('click',removeItem)
 clearAll.addEventListener('click',emptyList)
+
+checkUI();
