42062235380dd4ab62d434c14efbf320650c4eca
dddhiraj2001 <dddhiraj2001@gmail.com>
2024-02-16 19:00:35 -0500

    Added local storage functionality

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const itemForm = document.getElementById('item-form')
 const itemInput = document.getElementById('item-input')
 const itemList = document.getElementById('item-list')
 const clearAll = document.getElementById('clear')
-
 const filter = document.querySelector('.filter')
-const addItem = e => {
+const onaddItemSubmit = e => {
     e.preventDefault()
 
     if(itemInput.value === ''){
@@ -12,12 +11,33 @@ const addItem = e => {
         return 
     }
     console.log('Sucess');
-    const child = createDiv(itemInput.value)
+    addItemToDom(itemInput.value)
+    addItemToStorage(itemInput.value)
+    
+    
+}
+const addItemToDom = (input) =>{
+    const child = createDiv(input)
     itemList.appendChild(child)
     checkUI();
     // console.log(itemInput.value)
-    itemInput.value=''
-    
+    input='';
+
+}
+const addItemToStorage = (item) =>{
+    let itemsFromStorage;
+
+    if (localStorage.getItem('items') === null){
+        itemsFromStorage = []
+
+    }else{
+        itemsFromStorage = JSON.parse(localStorage.getItem('items'));
+    }
+     
+    itemsFromStorage.push(item);
+    localStorage.setItem('items',JSON.stringify(itemsFromStorage));
+
+        
 }
 const createDiv = shoppingItem =>{
 
@@ -44,14 +64,27 @@ const removeItem = e => {
     // console.log(e.target.parentElement)
     if (e.target.tagName === "I"){
         if(confirm('Are you sure')){
+        const item = e.target.parentElement.parentElement.innerText; 
         e.target.parentElement.parentElement.remove();
-        checkUI();
+        const temp = JSON.parse(localStorage.getItem('items'));
+        
+        temp.splice(temp.indexOf(item),1)
+        localStorage.setItem('items',JSON.stringify(temp))
+        const x = checkUI();
+        if(x===true){
+            localStorage.clear()
+        }
     }
+    
 }
 }
 const emptyList = () =>  {
     itemList.innerHTML = " " ;
-    checkUI();
+    const x = checkUI();
+    if(x===true){
+        localStorage.clear()
+    }
+
 }
 const checkUI = () => {
     const items = document.querySelectorAll('li')
@@ -59,13 +92,46 @@ const checkUI = () => {
     if(items.length===0){
         filter.style.display = 'none'
         clearAll.style.display = 'none'
+        return true
     }else{
         filter.style.display = 'block'
         clearAll.style.display = 'block'
     }
 }
-itemForm.addEventListener('submit',addItem)
-itemList.addEventListener('click',removeItem)
-clearAll.addEventListener('click',emptyList)
+const filterItems = (e)=>{
+    const items = document.querySelectorAll('li')
+    const input = e.target.value.toLowerCase();
+    
+    items.forEach((item)=>{
+        const itemName = item.innerText
+    
+        if (itemName.indexOf(input) != -1)
+        {
+            item.style.display = 'flex';
+        }else
+        {
+    item.style.display = 'none'
+    }
+        
+    })
+}
+const loadWindow = ()=>{
+    if(localStorage.getItem('items')!==null){
+    const names = JSON.parse(localStorage.getItem('items'));
+    names.forEach((item)=>{
+        console.log(item)
+        addItemToDom(item)
+    })
+}
+}
+itemForm.addEventListener('submit',onaddItemSubmit);
+itemList.addEventListener('click',removeItem);
+clearAll.addEventListener('click',emptyList);
+filter.addEventListener('input',filterItems);
+document.addEventListener('DOMContentLoaded',loadWindow)
 
+localStorage.setItem('name','Dev');
+console.log(localStorage.getItem('name'));
+// localStorage.removeItem('name');
+// localStorage.clear();
 checkUI();
